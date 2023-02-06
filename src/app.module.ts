9b5bfef14226bc64891568630127f036d9f6a29f
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-06 20:44:59 +0300

    refactor: update app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DBModule } from './db/db.module';
+import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [UserModule, DBModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    DBModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavoritesModule,
+  ],
 })
 export class AppModule {}
