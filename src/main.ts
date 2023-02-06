ddff1bc87e466644b84bb02882f18ff59cff2e49
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-07 02:32:53 +0300

    docs: update doc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './utils/constants';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('1.0.0')
+    .addTag('music library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(PORT);
 }
 
