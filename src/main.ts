246b2662fd394787cb92668046b2d70cd32f6434
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-06 23:30:19 +0300

    docs: update OpenAPI file

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { PORT } from './utils/constants';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   await app.listen(PORT);
 }
 
