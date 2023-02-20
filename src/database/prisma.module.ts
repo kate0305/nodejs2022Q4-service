3c88465f4c37c001a709229ff61169f2385dfb97
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-20 20:33:39 +0300

    fix: fix mistake

--- src/database/prisma.module.ts
+++ src/database/prisma.module.ts
@@ -3,6 +3,6 @@ import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [PrismaService],
-  controllers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
