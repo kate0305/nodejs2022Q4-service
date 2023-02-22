3bd8f00454dfa8e49fcf7d1e260c1d89c4ad0a53
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-22 12:41:00 +0300

    refactor: delete db in memory

--- src/db/db.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { Database } from './db';
-
-@Global()
-@Module({
-  providers: [Database],
-  exports: [Database],
-})
-export class DBModule {}
