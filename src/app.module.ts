9157cf63e28dcff2c4353f0e44447cb077fc589e
Katsiaryna Voronich <katarinka403@gmail.com>
2023-02-22 14:59:32 +0300

    refactor: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { DBModule } from './db/db.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
@@ -10,7 +9,6 @@ import { PrismaModule } from './database/prisma.module';
 @Module({
   imports: [
     UserModule,
-    DBModule,
     ArtistModule,
     AlbumModule,
     TrackModule,
